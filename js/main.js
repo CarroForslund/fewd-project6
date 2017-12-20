46a8ef7bb0dfd6fce94e59fab59f6ec5b8bf4efd
Caroline Forslund <cf222ev@student.lnu.se>
2017-12-21 00:09:08 +0100

    Trying to make the video event trigger in Safari...

--- js/main.js
+++ js/main.js
@@ -1,37 +1,79 @@
+// //Variable declaration
+// let video = document.getElementsByTagName('video')[0];
+// const spans = document.querySelectorAll('.transcript-wrapper span');
+//
+// //Highlight current part of transcript when video plays
+// video.addEventListener('timeupdate', () => {
+//   const currentTime = video.getCurrentTime();
+//
+//   for (let i = 0; i < spans.length; i++) {
+//     const video = document.getElementsByTagName('video')[0];
+//     const span = spans[i];
+//     const start = parseFloat(span.dataset.start);  //returns decimal number
+//     const end = parseFloat(span.dataset.end);
+//
+//     if (currentTime > start && currentTime < end) {
+//       span.classList.add("highlight");
+//     } else {
+//       span.classList.remove("highlight");
+//     }
+//   }
+// });
+//
+// //Link span sections to the right video sequence
+// for (let i = 0; i < spans.length; i++) {
+//   const span = spans[i];
+//   const start = parseFloat(span.dataset.start);
+//   const end = parseFloat(span.dataset.end);
+//
+//
+//   span.addEventListener('click', (event) => {
+//     video = document.getElementsByTagName('video')[0];
+//     if (span) {
+//       video.setCurrentTime(start);
+//       video.play();
+//     }
+//   });
+// };
+
 //Variable declaration
+let spans = document.querySelectorAll('.transcript-wrapper span');
 let video = document.getElementsByTagName('video')[0];
-const spans = document.querySelectorAll('.transcript-wrapper span');
 
 //Highlight current part of transcript when video plays
-video.addEventListener('timeupdate', () => {
+video.addEventListener('timeupdate', function(event) {
+  console.log("nice");
   const currentTime = video.getCurrentTime();
 
   for (let i = 0; i < spans.length; i++) {
-    const video = document.getElementsByTagName('video')[0];
-    const span = spans[i];
+    video = document.getElementsByTagName('video')[0];
+    span = spans[i];
     const start = parseFloat(span.dataset.start);  //returns decimal number
     const end = parseFloat(span.dataset.end);
 
     if (currentTime > start && currentTime < end) {
+      console.log("Highligt" + span);
       span.classList.add("highlight");
     } else {
+      console.log("NO Highligt" + span);
       span.classList.remove("highlight");
     }
   }
 });
 
+
 //Link span sections to the right video sequence
 for (let i = 0; i < spans.length; i++) {
-  const span = spans[i];
+  span = spans[i];
   const start = parseFloat(span.dataset.start);
   const end = parseFloat(span.dataset.end);
 
 
-  span.addEventListener('click', (event) => {
+  span.addEventListener('click', function (event) {
     video = document.getElementsByTagName('video')[0];
     if (span) {
       video.setCurrentTime(start);
       video.play();
     }
   });
-};
+}

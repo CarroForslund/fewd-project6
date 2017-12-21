be2d21f3e7e66d8f105dd5423cde8f7915727a83
Caroline Forslund <cf222ev@student.lnu.se>
2017-12-21 01:18:11 +0100

    HIghligths and links displays correctly
    In Firefox, Chrome and FINALLY also in Safari.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,3 @@
-// //Variable declaration
-// let video = document.getElementsByTagName('video')[0];
-// const spans = document.querySelectorAll('.transcript-wrapper span');
-//
-// //Highlight current part of transcript when video plays
-// video.addEventListener('timeupdate', () => {
-//   const currentTime = video.getCurrentTime();
-//
-//   for (let i = 0; i < spans.length; i++) {
-//     const video = document.getElementsByTagName('video')[0];
-//     const span = spans[i];
-//     const start = parseFloat(span.dataset.start);  //returns decimal number
-//     const end = parseFloat(span.dataset.end);
-//
-//     if (currentTime > start && currentTime < end) {
-//       span.classList.add("highlight");
-//     } else {
-//       span.classList.remove("highlight");
-//     }
-//   }
-// });
-//
-// //Link span sections to the right video sequence
-// for (let i = 0; i < spans.length; i++) {
-//   const span = spans[i];
-//   const start = parseFloat(span.dataset.start);
-//   const end = parseFloat(span.dataset.end);
-//
-//
-//   span.addEventListener('click', (event) => {
-//     video = document.getElementsByTagName('video')[0];
-//     if (span) {
-//       video.setCurrentTime(start);
-//       video.play();
-//     }
-//   });
-// };
-
 $('#mediaplayer').mediaelementplayer({
   stretching: 'responsive',
   features: ['playpause', 'current', 'progress', 'duration', 'volume', 'fullscreen'],
@@ -47,20 +9,17 @@ let video = document.getElementsByTagName('video')[0];
 
 //Highlight current part of transcript when video plays
 video.addEventListener('timeupdate', function(event) {
-  console.log("nice");
-  const currentTime = video.getCurrentTime();
+  const currentTime = video.getCurrentTime(); //recognizes where in the video we are
 
   for (let i = 0; i < spans.length; i++) {
     video = document.getElementsByTagName('video')[0];
-    span = spans[i];
-    const start = parseFloat(span.dataset.start);  //returns decimal number
+    const span = spans[i];
+    startTime = parseFloat(span.dataset.start);  //returns decimal number
     const end = parseFloat(span.dataset.end);
 
-    if (currentTime > start && currentTime < end) {
-      console.log("Highligt" + span);
+    if (currentTime > startTime && currentTime < end) {
       span.classList.add("highlight");
     } else {
-      console.log("NO Highligt" + span);
       span.classList.remove("highlight");
     }
   }
@@ -69,16 +28,15 @@ video.addEventListener('timeupdate', function(event) {
 
 //Link span sections to the right video sequence
 for (let i = 0; i < spans.length; i++) {
-  span = spans[i];
+  const span = spans[i];
   const start = parseFloat(span.dataset.start);
   const end = parseFloat(span.dataset.end);
-
+  console.log("start");
 
   span.addEventListener('click', function (event) {
-    video = document.getElementsByTagName('video')[0];
-    if (span) {
-      video.setCurrentTime(start);
-      video.play();
-    }
+    // video = document.getElementsByTagName('video')[0];
+    console.log(start);
+    video.setCurrentTime(start); //Tels where to start playing video
+    video.play();
   });
 }
